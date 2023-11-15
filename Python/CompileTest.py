4c47c52e52e57bc88c61db994e832cb6e679f751
Kinesis <lordkinesis@naver.com>
2023-11-15 16:15:42 +0900

    Directory Edit

--- Python/CompileTest.py
+++ Python/CompileTest.py
@@ -1,2 +1,8 @@
-a, b = input().split(" ")
-print(int(a) % int(b))
+N = int(input())
+numberList = list(map(int, input().split()))
+v = int(input())
+cnt = 0
+for x in range(N):
+    if numberList[x] == v:
+        cnt = cnt + 1
+print(cnt)
