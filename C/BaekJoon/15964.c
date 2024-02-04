ec50b5d3c15508d36acea1c80fd1e2a95d60f96f
Kinesis <lordkinesis@naver.com>
2024-02-04 18:33:02 +0900

    15964 Update

--- C/BaekJoon/15964.c
+++ C/BaekJoon/15964.c
@@ -1,18 +1,18 @@
 #include <math.h>
 #include <stdio.h>
 
-int Function(int A, int B);
+int Function(int n1, int n2, int n3, int n4, int n5);
 
 int main() {
-    long long n1, n2;
-    scanf("%lld %lld", &n1, &n2);
-    Function(n1, n2);
+    int n1, n2, n3, n4, n5;
+    scanf("%d %d %d %d %d", &n1, &n2, &n3, &n4, &n5);
+    Function(n1, n2, n3, n4, n5);
     return 0;
 }
 
-int Function(int A, int B){
-
+int Function(int n1, int n2, int n3, int n4, int n5){
     int result = 0;
-    result = pow(A, 2) - pow(B, 2);
-    printf("%lld", result);
+    result = (pow(n1, 2) + pow(n2, 2) + pow(n3, 2) + pow(n4, 2) + pow(n5, 2));
+    result = result % 10;
+    printf("%d", result);
 }
