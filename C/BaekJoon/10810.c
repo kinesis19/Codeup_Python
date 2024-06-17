721d2a7944749a7c117da94defbf401d9dd816d1
Kinesis <lordkinesis@naver.com>
2024-06-18 00:34:30 +0900

    Update 10810.c

--- C/BaekJoon/10810.c
+++ C/BaekJoon/10810.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 int main(){
+    int ary[100] = {0, };
+    int N, M;
+    int a, b;
+    int i, j, k;
+    scanf("%d %d", &N, &M);
 
-
-    return 0;
+    for(a = 0; a < M; a++){
+        scanf("%d %d %d", &i, &j, &k);
+        for(b = i; b <= j;b++){
+            ary[b-1] = k;
+        }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for(a = 0; a < N; a++){
+        printf("%d ", ary[a]);
+    }
+    return 0;
+}
