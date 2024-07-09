e4661f9a74919fb292f165f56ba4458dacd7e5f5
Kinesis <lordkinesis@naver.com>
2024-07-09 16:18:02 +0900

    Update:
    - 24078.c
    - 24086.c
    - 6840.c

---
+++ C/BaekJoon/24078.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+    scanf("%d", &num);
+
+    printf("%d", num%21);
+    
+    return 0;
+}
---
+++ C/BaekJoon/6840.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1, num2, num3;
+    scanf("%d\n%d\n%d", &num1, &num2, &num3);
+
+    if((num1 >= num2 && num2 >= num3) || (num3 >= num2 && num2 >= num1)){
+        printf("%d", num2);
+    }else if((num2 >= num1 && num1 >= num3) || (num3 >= num1 && num1 >= num2)){
+        printf("%d", num1);
+    }else if((num1 >= num3 && num3 >= num2) || (num2 >= num3 && num3 >= num1)){
+        printf("%d", num3);
+    }
+
+    return 0;
+}
