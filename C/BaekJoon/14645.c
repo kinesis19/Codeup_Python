6b2e2d72998a49db672614a2231f192c3745479c
Kinesis <lordkinesis@naver.com>
2024-07-09 14:58:59 +0900

    Update:
    - 14645.c
    - 15680.c
    - 15733.c

---
+++ C/BaekJoon/14645.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1, numIn, numOut;
+    int i;
+
+    scanf("%d %d", &num1, &numIn);
+
+    for(i = 0; i < num1; i++){
+        scanf("%d %d", &numIn, &numOut);
+    }
+    printf("ºñ¿ÍÀÌ");
+
+    return 0;
+}
---
+++ C/BaekJoon/15680.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+    scanf("%d", &num);
+
+    printf("%s", num == 0 ? "YONSEI" : "Leading the Way to the Future");
+    
+    return 0;
+}
